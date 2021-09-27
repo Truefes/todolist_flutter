cbb355f3286445fc37ce8f3fe65523ad6039d2b4
Truefes <yosv002@gmail.com>
2021-09-28 06:21:26 +0800

    created truefesTut branch

--- todolist_app/lib/main.dart
+++ todolist_app/lib/main.dart
@@ -9,8 +9,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
+      home: Scaffold(        
+        backgroundColor: Colors.grey[300],
+        endDrawer: Drawer(),
+        appBar: AppBar(
+          title: Text("To do List"),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+          leading: IconButton(icon:Icon (Icons.arrow_back), onPressed: () {ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('This is a back button')));
+            },
+          ),
+          iconTheme: IconThemeData(color: Colors.white),
+          
+        ),
+        body:Container(
+         decoration: BoxDecoration(
+            image: DecorationImage(
+               image: NetworkImage('https://c.tenor.com/zYdnJeHy-ZoAAAAM/red-panda-truly.gif'), fit: BoxFit.contain,
+            ),
+         ),
+      ) ,
+
       ),
     );
   }
